a3a576fd8cd19d88d6f10c4bcdab8faf436c1517
ailtmy <takase@mitsuwa-off.jp>
2020-11-18 15:12:09 +0900

    create app

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { TextField } from "@material-ui/core";
 
+import { pushMessage } from "../firebase";
+
 const MessageField = ({ name, setText, text }) => {
     const [isComposed, setIsComposed] = useState(false);
     return (
@@ -14,7 +16,7 @@ const MessageField = ({ name, setText, text }) => {
                 if (text === "") return;
 
                 if (e.key === "Enter") {
-                    console.log("push message to firebase");
+                    pushMessage({ name: "はむさん", text });
                     setText("");
                     e.preventDefault();
                 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAZY4c1dOGHXxMenGAqE_ZtIvEtgOTDGeE",
+    authDomain: "idobatakaigi-with-ham-1fa71.firebaseapp.com",
+    databaseURL: "https://idobatakaigi-with-ham-1fa71.firebaseio.com",
+    projectId: "idobatakaigi-with-ham-1fa71",
+    storageBucket: "idobatakaigi-with-ham-1fa71.appspot.com",
+    messagingSenderId: "256111363111",
+    appId: "1:256111363111:web:b79370e88ab2445fa2fa02",
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref("messages");
+
+export const pushMessage = ({ name, text }) => {
+    messageRef.push({ name, text });
+};
